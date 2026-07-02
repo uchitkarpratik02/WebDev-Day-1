50d12546be62118c96eb9a9e97df860fe6170798
Pratik Uchitkar <uchitkarpratik02@gmail.com>
2026-07-02 23:54:56 +0530

    Add files via upload

---
+++ Day-14/mini-project-03/script.js
@@ -0,0 +1,11 @@
+console.log("Hello!") 
+let Name="My name is Pratik.";
+let Age = "I am 22 years old.";
+let City="I live in Pune.";
+let Skills="Currently learning MERN Stack.";
+let DreamJob="My dream job is Full Stack Developer.";
+console.log(Name) 
+console.log(Age) 
+console.log(City) 
+console.log(Skills) 
+console.log(DreamJob) 
